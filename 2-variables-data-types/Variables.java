bd7d1795b3a4b568c1d120d5283314de1162def0
Erkan <erkansoftwaredeveloper@gmail.com>
2026-02-11 08:50:06 +0100

    Expand Variables class with additional data types
    Added examples of double, string, boolean, and constants.

--- 2-variables-data-types/Variables.java
+++ 2-variables-data-types/Variables.java
@@ -1,9 +1,27 @@
 public class Variables {
   public static void main(String[] args) {
 
-    // Integer
+    // Integer (whole number (no decimals))
     int age = 25;
     System.out.println("Age: " + age); // Age: 25
-      
+
+    // Double (Used to store decimal numbers)
+    double pi = 3.1415;
+    System.out.println("Pi: " + pi); // Pi: 3.1415
+
+    // String (Used to store Text)
+    String name = "Erkan";
+    System.out.println("Name: " + name); // Name: Erkan
+
+    // Boolean (true/false) values
+    boolean isStudent = true;
+    System.out.println("Is student? " + isStudent); //Is student? True
+
+    // Constants (makes a variable unchangeable)
+    final int DAYS_IN_WEEK = 7;
+    System.out.println("Days in a week: " + DAYS_IN_WEEK); // Days in a week: 7
+
+    
   }
 }
+
