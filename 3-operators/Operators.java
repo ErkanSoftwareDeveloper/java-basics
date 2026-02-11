709f0f1cfe8ac9a9ee32805d55a37554d0978d2c
Erkan <erkansoftwaredeveloper@gmail.com>
2026-02-11 09:50:54 +0100

    Add arithmetic operations in Operators.java
    Implement basic arithmetic operations and demonstrate their usage.

---
+++ 3-operators/Operators.java
@@ -0,0 +1,23 @@
+public class Operators {
+  public static void main(String[] args) {
+
+    // Arithmetic Operators
+    int a = 10;
+    int b = 3;
+    System.out.println("a + b = " + (a + b)); // 13
+    System.out.println("a - b = " + (a - b)); // 7
+    System.out.println("a * b = " + (a * b)); // 30  
+    System.out.println("a / b = " + (a / b)); // 3 int  
+    System.out.println("a % b = " + (a % b)); // 1 (remainder)
+    a++;  
+    System.out.println("a++ = " + a); // 11
+    b--;
+    System.out.println("b-- = " + b); // 2
+
+    // Double division
+    double result = (double)a / b;
+    System.out.println("Division (double): " + result); // Division (double) : 5.5 (11 / 2 = 5.5)
+
+    // Comparison Operator
+    
+  }
