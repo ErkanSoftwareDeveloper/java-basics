7e296a2870d28eb142b0fd5e702a99ffc966df19
Erkan <erkansoftwaredeveloper@gmail.com>
2026-02-15 10:12:29 +0100

    Add comparison, logical, and assignment operators

--- 3-operators/Operators.java
+++ 3-operators/Operators.java
@@ -19,5 +19,44 @@ public class Operators {
     System.out.println("Division (double): " + result); // Division (double) : 5.5 (11 / 2 = 5.5)
 
     // Comparison Operator
+    System.out.println(x == y);
+    System.out.println(x != y);
+    System.out.println(x > y);
+    System.out.println(x < y);
+    System.out.println(x >= 5);
+    System.out.println(y <= 10);
+
+    // Logical Operators
+    boolean a = true;
+    boolean b = false;
+    System.out.println(a && b);
+    System.out.println(a || b);
+    System.out.println(!a);
+
+    // Assignment Operators
+    int number = 5;
+    
+    number += 3; // 8 (5+3=8)
+    System.out.println(number);
+    
+    number -= 2; // 6 (8-2=6)
+    System.out.println(number);
+    
+    number *= 2; // 12 (6*2=12)
+    System.out.println(number);
+    
+    number /= 3; // 4 (12/3=4)
+    System.out.println(number);
+    
+    number %= 3; // 1 (4/3=1)
+    System.out.println(number);
     
   }
+
+/* Arithmetic: + - * / % ++ --
+
+Comparison: == != > < >= <=
+
+Logical: && || !
+
+Assignment: = += -= *= /= %=                     */
