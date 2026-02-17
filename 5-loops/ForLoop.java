a1c9278dcf7d7c10c8336bf51a9e4f61bba2bf65
Erkan <erkansoftwaredeveloper@gmail.com>
2026-02-17 19:20:45 +0100

    Add ForLoop class to print numbers 1 to 5
    Implement a for loop to print numbers from 1 to 5.

---
+++ 5-loops/ForLoop.java
@@ -0,0 +1,12 @@
+// A for loop is used when you want to repeat something a specific number of times.
+
+// example 1: print 1 to 5
+
+public class ForLoop {
+  public static void main(String[] args) {
+
+    for (int i = 1; i <= 5; i++) {
+      System.out.println(i);
+    }
+  }
+}
