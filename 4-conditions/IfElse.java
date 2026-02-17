42fb21c55ea93583b506ec626fcce660d852546f
Erkan <erkansoftwaredeveloper@gmail.com>
2026-02-17 19:18:00 +0100

    Enhance IfElse.java with multiple examples
    Added examples for if-else statements and logical operators.

--- 4-conditions/IfElse.java
+++ 4-conditions/IfElse.java
@@ -5,18 +5,55 @@ In simple words:
 👉 If something is true → do this
 👉 Otherwise → do something else */
 
-publi class IfElse {
+public class IfElse {
   public static void main(String[] args) {
       
     // Example 1: simle if
   int age = 20;
 
   if (age >= 18) {
-    System.out.println("You are an adult.");
+    System.out.println("You are an adult."); // You are an adult.
   }
 
   System.out.println("------------------");
+
+    // Example 2: if - else
+    int number = 7;
+
+    if (number % 2 == 0) { 
+      System.out.println("The number is even.");
+    } else {
+      System.out.println("The number is odd."); // (7/2=3 rest 1!) The number is odd
+    }
+
+    System.out.println("___________");
+
+      // Example 3: if - else if - else
+    int score = 85;
+
+    if (score >= 90) {
+        System.out.println("Grade: A"):
+    } else if (score >= 80) {
+        System.out.println("Grade: B"); // Grade: B
+    } else if (score >= 80) {
+        System.out.println("Grade: C");
+    } else {
+      System.out.println("Grade: F");
+    }
+
+    System.out.pritln("_________________");
+
+    // Example 4: Using logical operators
+    boolean hasLicense = true;
+    int age = 18;
+
+    if (age >= 18 && hasLicense) { 
+      System.out.println("You can drive."); // you can drive.
+    } else {
+      System.out.println("You cant drive.");
+    }
   }
 }
+
 
 
